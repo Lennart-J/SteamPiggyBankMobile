70ecde00059a117d3ca41726852b7861a3de189b
Lennart-J <l.jarms@hotmail.de>
2014-11-18 23:08:25 +0100

    nightly

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,8 +7,101 @@ angular.module('SteamPiggyBank.controllers', ['ngAnimate'])
 	}).then(function(popover) {
 		$scope.popover = popover;
 	});
-	var promise = requestService.getAllApps();
-	promise.then(function(data) {
+	var mainSlider = angular.element(document.querySelector('.slider-slides')),
+		swipeGesture = null;
+
+	//DEBUG
+	$scope.dailyDail = {
+		appid: "313740",
+		discount: "-66%",
+		finalprice: "3,39€",
+		originalprice: "9,99€",
+		packageid: undefined,
+		timeremaining: ""
+	};
+	$scope.featuredDeals = [{
+		appid: null,
+		discount: 85,
+		finalprice: 599,
+		imageUrl: "http://cdn.akamai.steamstatic.com/steam/subs/19282/capsule_616x353.jpg?t=1397250555",
+		name: "Tropico 4 Collector's Bundle",
+		originalprice: 3999,
+		packageid: "19282"
+	}, {
+		appid: null,
+		discount: 85,
+		finalprice: 599,
+		imageUrl: "http://cdn.akamai.steamstatic.com/steam/apps/245170/capsule_616x353.jpg?t=1412184944",
+		name: "Tropico 4 Collector's Bundle",
+		originalprice: 3999,
+		packageid: "19282"
+	}];
+	$scope.appItems = [{
+		appid: "57690",
+		discount: "-85%",
+		finalprice: "5,99€",
+		imageUrl: "http://cdn.akamai.steamstatic.com/steam/apps/57690/capsule_184x69.jpg",
+		name: "Tropico 4: Steam Special Edition",
+		originalprice: "39,99€ ",
+		packageid: undefined,
+		released: "1 Sep, 2011",
+		urcClass: "positive",
+		urcScore: 3623.1800000000003,
+		urcText: "Very Positive<br>93% of the 4,213 user reviews for this game are positive."
+	}, {
+		appid: "57690",
+		discount: "-100%",
+		finalprice: "55,99€",
+		imageUrl: "http://cdn.akamai.steamstatic.com/steam/apps/57690/capsule_184x69.jpg",
+		name: "Tropico 4: Steam Special Edition",
+		originalprice: "39,99€ ",
+		packageid: undefined,
+		released: "1 Sep, 2011",
+		urcClass: "positive",
+		urcScore: 3623.1800000000003,
+		urcText: "Very Positive<br>93% of the 4,213 user reviews for this game are positive."
+	}, {
+		appid: "57690",
+		discount: "-10%",
+		finalprice: "55,99€",
+		imageUrl: "http://cdn.akamai.steamstatic.com/steam/apps/57690/capsule_184x69.jpg",
+		name: "Tropico 4: Steam Special Edition",
+		originalprice: "39,99€ ",
+		packageid: undefined,
+		released: "1 Sep, 2011",
+		urcClass: "positive",
+		urcScore: 3623.1800000000003,
+		urcText: "Very Positive<br>93% of the 4,213 user reviews for this game are positive."
+	}, {
+		appid: "57690",
+		discount: "-50%",
+		finalprice: "55,99€",
+		imageUrl: "http://cdn.akamai.steamstatic.com/steam/apps/57690/capsule_184x69.jpg",
+		name: "Tropico 4: Steam Special Edition",
+		originalprice: "39,99€ ",
+		packageid: undefined,
+		released: "1 Sep, 2011",
+		urcClass: "positive",
+		urcScore: 3623.1800000000003,
+		urcText: "Very Positive<br>93% of the 4,213 user reviews for this game are positive."
+	}];
+
+	var promiseDailyDeal = requestService.getFrontPageDeals();
+	promiseDailyDeal.then(function(data) {
+		$scope.dailyDeal = data;
+		console.log("DailyDeal: ", data);
+	});
+
+	var promiseFeaturedDeals = requestService.getFeaturedDeals();
+	promiseFeaturedDeals.then(function(data) {
+		$scope.featuredDeals = data;
+
+		// $ionicSlideBoxDelegate.$getByHandle('gallery-slider').update();
+		console.log("FeaturedDeals: ", data);
+	});
+
+	var promiseAllApps = requestService.getAllApps();
+	promiseAllApps.then(function(data) {
 		console.log(data);
 	}, function(reason) {
 		console.log(reason);
@@ -16,18 +109,17 @@ angular.module('SteamPiggyBank.controllers', ['ngAnimate'])
 		console.log(update);
 		$scope.appItems = update;
 	});
-	var mainSlider = angular.element(document.querySelector('.slider-slides')),
-		swipeGesture = null;
+
 
 	// Called to navigate to the main app
 	$scope.startApp = function() {
 		$state.go('main');
 	};
 	$scope.next = function() {
-		$ionicSlideBoxDelegate.select( $ionicSlideBoxDelegate.next() );
+		$ionicSlideBoxDelegate.select($ionicSlideBoxDelegate.next());
 	};
 	$scope.previous = function() {
-		$ionicSlideBoxDelegate.select( $ionicSlideBoxDelegate.previous() );
+		$ionicSlideBoxDelegate.select($ionicSlideBoxDelegate.previous());
 	};
 	$scope.slide = function(index) {
 		$ionicSlideBoxDelegate.select(index);
@@ -82,40 +174,6 @@ angular.module('SteamPiggyBank.controllers', ['ngAnimate'])
 		$ionicNavBarDelegate.setTitle(title);
 	};
 
-	$scope.appList = [{
-		'id': 286140,
-		'title': 'Title1',
-		'price': 'Price'
-	}, {
-		'id': 286140,
-		'title': 'Title2',
-		'price': 'Price'
-	}, {
-		'id': 286140,
-		'title': 'Title3',
-		'price': 'Price'
-	}, {
-		'id': 286140,
-		'title': 'Title4',
-		'price': 'Price'
-	}, {
-		'id': 286140,
-		'title': 'Title5',
-		'price': 'Price'
-	}, {
-		'id': 286140,
-		'title': 'Title6',
-		'price': 'Price'
-	}, {
-		'id': 286140,
-		'title': 'Title7',
-		'price': 'Price'
-	}, {
-		'id': 286140,
-		'title': 'Title8',
-		'price': 'Price'
-	}];
-
 	$scope.toggleLeft = function() {
 		$ionicSideMenuDelegate.toggleLeft();
 	};
@@ -131,7 +189,6 @@ angular.module('SteamPiggyBank.controllers', ['ngAnimate'])
 	});
 
 	$scope.$watch($ionicSlideBoxDelegate.selected, function(index) {
-
 		if (index === 0) {
 			swipeGesture = $ionicGesture.on('swiperight', showSideMenuOnSwipe, mainSlider);
 		} else if (swipeGesture !== null) {
@@ -150,13 +207,13 @@ angular.module('SteamPiggyBank.controllers', ['ngAnimate'])
 
 .controller('GalleryCtrl', function($scope, $ionicSlideBoxDelegate) {
 	$scope.showNext = function() {
-		$ionicSlideBoxDelegate.next();
+		$ionicSlideBoxDelegate.select( $ionicSlideBoxDelegate.next() );
 	};
 	$scope.showPrev = function() {
-		$ionicSlideBoxDelegate.previous();
+		$ionicSlideBoxDelegate.select( $ionicSlideBoxDelegate.previous() );
 	};
 	$scope.slide = function(index) {
-		$ionicSlideBoxDelegate.slide(index);
+		$ionicSlideBoxDelegate.select(index);
 	};
 	$scope.slideChanged = function(index) {
 		$scope.slideIndex = index;
@@ -179,8 +236,7 @@ angular.module('SteamPiggyBank.controllers', ['ngAnimate'])
 	}];
 
 	$scope.getDealName = function() {
-		var weekday = 'Hallo';
-		return weekday;
+		return "Featured Deals";
 		/*if (weekday.toString() === 'Sunday' || 'Saturday' || 'Friday') {
 			return 'Weekend Deals';
 		} else {
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -9,7 +9,7 @@ angular.module('SteamPiggyBank.services', [])
 			parent = {},
 			currentPage = 1,
 			maxPage = 0,
-			tmp_list = [],
+			tmpList = [],
 			allItemsOnSale = [],
 			$data,
 			status = 0,
@@ -22,12 +22,12 @@ angular.module('SteamPiggyBank.services', [])
 				$data = $(data.replace(/<img\b[^>]*>/ig, ''));
 				parent = $data.find('#search_result_container');
 				maxPage = findLastSalePage($data);
-				tmp_list = findSaleItems(parent);
+				tmpList = findSaleItems(parent);
 				allUserTags = findAllUserTags($data);
 
-				allItemsOnSale = allItemsOnSale.concat(parseDOMElementList(tmp_list));
+				allItemsOnSale = allItemsOnSale.concat(parseDOMElementList(tmpList));
 				currentPage++;
-				tmp_list = [];
+				tmpList = [];
 
 				for (; currentPage <= maxPage; currentPage++) {
 					XHRs.push(
@@ -35,13 +35,14 @@ angular.module('SteamPiggyBank.services', [])
 						.success(function(data) {
 							$data = $(data.replace(/<img\b[^>]*>/ig, ''));
 							parent = $data.find('#search_result_container');
-							tmp_list = findSaleItems(parent);
-							allItemsOnSale = allItemsOnSale.concat(parseDOMElementList(tmp_list));
+							tmpList = findSaleItems(parent);
+							allItemsOnSale = allItemsOnSale.concat(parseDOMElementList(tmpList));
 							console.log(allItemsOnSale);
 							defer.notify(allItemsOnSale);
-							tmp_list = [];
+							tmpList = [];
 						})
 					);
+					//TODO resolve when all done
 				}
 			});
 
@@ -50,6 +51,38 @@ angular.module('SteamPiggyBank.services', [])
 		return defer.promise;
 	};
 
+	this.getFrontPageDeals = function() {
+		var $data,
+			defer = $q.defer(),
+			dailyDeal = {};
+
+		$http.get('http://store.steampowered.com/?forceMobile=0')
+			.success(function(data) {
+				$data = $(data.replace(/<img\b[^>]*>/ig, ''));
+				// parent = $data.find('#search_result_container');
+
+				dailyDeal = findDailyDeal($data);
+				defer.resolve(dailyDeal);
+			});
+
+		return defer.promise;
+	};
+
+	this.getFeaturedDeals = function() {
+		var defer = $q.defer(),
+			featuredDeals = [];
+
+		$http.get('http://store.steampowered.com/api/featuredcategories/')
+			.success(function(data) {
+				featuredDeals = parseFeaturedDeals(data);
+				defer.resolve(featuredDeals);
+			});
+
+		return defer.promise;
+	};
+
+
+	//private functions
 	var findLastSalePage = function(parent) {
 		return parent.find('.pagebtn').prev().html();
 	};
@@ -59,14 +92,14 @@ angular.module('SteamPiggyBank.services', [])
 	};
 
 	var findAllUserTags = function(parent) {
-		var tag_array = [],
-			tag_elements = parent.find('#TagFilter_Container .tab_filter_control');
+		var tagArray = [],
+			tagElements = parent.find('#TagFilter_Container .tab_filter_control');
 
-		$.each(tag_elements, function(key, el) {
-			tag_array.push($(el).data('loc'));
+		$.each(tagElements, function(key, el) {
+			tagArray.push($(el).data('loc'));
 		});
 
-		return tag_array;
+		return tagArray;
 	};
 
 	var parseDOMElementList = function(list) {
@@ -81,12 +114,14 @@ angular.module('SteamPiggyBank.services', [])
 			appitem.packageid = getPackageId($el);
 			appitem.name = getName($el);
 			appitem.released = getReleaseDate($el);
-			appitem.oldprice = getOldPrice($el);
-			appitem.newprice = getNewPrice($el);
+			appitem.originalprice = getOriginalPrice($el);
+			appitem.finalprice = getFinalPrice($el);
 			appitem.discount = getDiscount($el);
 			appitem.urcText = urcText;
 			appitem.urcScore = getUrcScore(urcText);
 			appitem.urcClass = getUserReviewScoreClass($el);
+			appitem.imageUrl = appitem.packageid ? getPackageImage(appitem.packageid) : getAppImage(appitem.appid);
+
 
 
 			appitems.push(appitem);
@@ -94,8 +129,6 @@ angular.module('SteamPiggyBank.services', [])
 		});
 
 		return appitems;
-
-
 
 		function getAppId(element) {
 			return element.data('dsAppid').toString();
@@ -114,11 +147,11 @@ angular.module('SteamPiggyBank.services', [])
 			return element.find('.search_released').html();
 		}
 
-		function getOldPrice(element) {
+		function getOriginalPrice(element) {
 			return element.find('strike').html();
 		}
 
-		function getNewPrice(element) {
+		function getFinalPrice(element) {
 			return element.find('.search_price.discounted').contents()
 				.filter(function() {
 					return this.nodeType === Node.TEXT_NODE;
@@ -129,12 +162,24 @@ angular.module('SteamPiggyBank.services', [])
 			return element.find('.search_discount span').text();
 		}
 
+		function getAppImage(appid) {
+			return "http://cdn.akamai.steamstatic.com/steam/apps/" + appid + "/capsule_184x69.jpg";
+		}
+
+		function getPackageImage(packageid) {
+			return "http://cdn.akamai.steamstatic.com/steam/subs/" + packageid + "/capsule_sm_120.jpg";
+		}
+
 		function getUserReviewScoreClass(element) {
 			var el = element.find('.search_reviewscore span');
 
-			if (el.hasClass('positive')) return 'positive';
-			else if (el.hasClass('positive')) return 'mixed';
-			else if (el.hasClass('negative')) return 'negative';
+			if (el.hasClass('positive')) {
+				return 'positive';
+			} else if (el.hasClass('positive')) {
+				return 'mixed';
+			} else if (el.hasClass('negative')) {
+				return 'negative';
+			}
 		}
 
 		function getUserReviewScoreText(element) {
@@ -156,7 +201,9 @@ angular.module('SteamPiggyBank.services', [])
 			function findUrcCount(str) {
 				var pattern = /\d+(,)?\d+/g,
 					result = pattern.exec(str);
-				if (result !== null) return parseInt(result[0].replace(',', ''));
+				if (result !== null) {
+					return parseInt(result[0].replace(',', ''));
+				}
 			}
 		}
 
@@ -172,4 +219,76 @@ angular.module('SteamPiggyBank.services', [])
 		}
 	};
 
+	var findDailyDeal = function(page) {
+		var parent = page.find('.dailydeal_ctn'),
+			dealitem = {};
+
+		console.log(parent, page);
+		// dealitem.name = getName(parent);
+		dealitem.appid = getAppId(parent);
+		dealitem.packageid = getPackageId(parent);
+		dealitem.originalprice = getOriginalPrice(parent);
+		dealitem.finalprice = getFinalPrice(parent);
+		dealitem.discount = getDiscountPercent(parent);
+		dealitem.timeremaining = getTimeRemaining(parent);
+
+		return dealitem;
+
+		function getAppId(parent) {
+			console.log(parent.find('.dailydeal_cap'));
+			return parent.find('.dailydeal_cap').data('dsAppid').toString();
+		}
+
+		function getPackageId(parent) {
+			var pid = parent.find('dailydeal_cap').data('dsPackageid');
+			return pid ? pid.toString() : undefined;
+		}
+
+		function getOriginalPrice(parent) {
+			return parent.find('.discount_original_price').html();
+		}
+
+		function getFinalPrice(parent) {
+			return parent.find('.discount_final_price').html();
+		}
+
+		function getDiscountPercent(parent) {
+			return parent.find('.discount_pct').html();
+		}
+
+		function getTimeRemaining(parent) {
+			return parent.find('.dailydeal_countdown').html();
+		}
+	};
+
+	var parseFeaturedDeals = function(json) {
+		var featuredDeals = [];
+
+		if (json.specials && json.specials.items) {
+			$.each(json.specials.items, function(key, el) {
+				var deal = {};
+
+				if (el.discounted) {
+					if (el.type === 0) {
+						deal.appid = el.id;
+						deal.packageid = null;
+					} else if (el.type === 1) {
+						deal.packageid = el.id;
+						deal.appid = null;
+					}
+					deal.name = el.name;
+					deal.originalprice = el.original_price;
+					deal.finalprice = el.final_price;
+					deal.discount = el.discount_percent;
+					deal.imageUrl = el.large_capsule_image;
+
+
+					featuredDeals.push(deal);
+				}
+
+			});
+		}
+
+		return featuredDeals;
+	};
 });
